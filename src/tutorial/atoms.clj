c12d5de4cf70e45262c2df4de532dd8b1fb549be
Mussi <mussi@MacBook-Air-de-Mussi.local>
2019-07-06 20:46:15 -0300

    data types 'atom'

---
+++ src/tutorial/atoms.clj
@@ -0,0 +1,19 @@
+(ns tutorial.atoms)
+
+(defn Atoms []
+  (def amount (atom 100))
+  (println @amount)
+
+  (swap! amount inc)
+  (println @amount)
+
+  (reset! amount 110)
+  (println @amount)
+
+  (compare-and-set! amount 110 120)
+  (println @amount)
+
+  (compare-and-set! amount 110 150)
+  (println @amount))
+
+(Atoms)
